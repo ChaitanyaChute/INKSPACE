b2cbf6b5041f33412090461709f1d18df8f3649b
Chaitanya-chute <chaitanyachute11@gmail.com>
2026-02-21 13:18:20 +0530

    created common package for zod

---
+++ packages/common/src/types.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+export const signupSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters").max(20, "Username must be at most 20 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string()
+        .min(6, "Password must be at least 6 characters")
+        .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+        .regex(/[0-9]/, "Password must contain at least one number")
+        .regex(/[@$!%*?&]/, "Password must contain at least one special character")
+});
+
+
+export const signinSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters").max(20, "Username must be at most 20 characters"),
+    password: z.string()
+});
+
+export const roomSchema = z.object({
+    name:z.string().min(3).max(20)
+
+})
