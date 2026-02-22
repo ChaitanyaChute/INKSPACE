f7c9530a6c5aa31ce436c9cd7aaa3c90661183b2
Chaitanya-chute <chaitanyachute11@gmail.com>
2026-02-22 12:12:40 +0530

    ws room logic added

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,106 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { prismaClient } from "@repo/database/client";
+import { JwT_SECRET } from '@repo/backend-common/config';
 
-const wss = new WebSocketServer({port:8080});
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection' , function connection(ws){
-    ws.on('message', function message(data) {
-        ws.send('pong'); 
-    });
+interface User {
+  ws: WebSocket,
+  rooms: string[],
+  userId: string
+}
+
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+  try {
+    const decoded = jwt.verify(token, JwT_SECRET);
+
+    if (typeof decoded === "string") {
+      return null;
+    }
+
+    if (!decoded || !decoded.userId) {
+      return null;
+    }
+
+    return decoded.userId;
+  } catch(e) {
+    return null;
+  }
+}
+
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);  
+  //this will split url into array and get the query part 
+  // for example if url is www.chait8nya.me/?token=abc then it will split into ["www.chait8nya.me/","token=abc"] 
+  // and we will get the second part which is query string and then we will parse it using URLSearchParams to get the token value
+  const token = queryParams.get('token') || "";
+  const userId = checkUser(token);
+
+  if (userId === null) {
+    ws.close()
+    return;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws
+  })
+
+  ws.on('message', async function message(data) {
+    let parsedData;
+    if (typeof data !== "string") {
+      parsedData = JSON.parse(data.toString());
+    } else {
+      parsedData = JSON.parse(data); 
+    }
+
+    if (parsedData.type === "join_room") {
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type === "leave_room") {
+      const user = users.find(x => x.ws === ws);
+      if (!user) {
+        return;
+      }
+      user.rooms = user?.rooms.filter(x => x === parsedData.room);
+    }
+
+    console.log("message received")
+    console.log(parsedData);
+
+    if (parsedData.type === "chat") {
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
+
+      await prismaClient.chat.create({
+        data: {
+          roomId: Number(roomId),
+          message,
+          userId
+        }
+      });
+
+      users.forEach(user => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(JSON.stringify({
+            type: "chat",
+            message: message,
+            roomId
+          }))
+        }
+      })
+    }
+
+  });
 
 });
